6d93ef9c1a7d4debc7fbaefc9d25d5b69b507ad4
Edward Kim <eddotman@gmail.com>
2022-03-14 21:27:37 -0400

    add a physicality test

--- 02_testing_models/physicality_tests.py
+++ 02_testing_models/physicality_tests.py
@@ -28,3 +28,25 @@ def test_stoichiometric_monotonicity(input_series:str) -> None:
     ]
 
     assert predicted_values == sorted(predicted_values, reverse=True)
+
+@pytest.mark.parametrize(
+    "input_val",
+    [
+        [
+            "MgV2O4",
+            "O4MgV2",
+            "V2MgO4",
+        ],
+        [
+            "Li3(WO3)8",
+            "(WO3)8Li3",
+        ]
+    ]
+)
+def test_atomic_order(input_series:str) -> None:
+    predicted_values = [
+        synthesizability_model.predict_single(input_val)
+        for input_val in input_series
+    ]
+
+    assert max(predicted_values) - min(predicted_values) < 0.001
