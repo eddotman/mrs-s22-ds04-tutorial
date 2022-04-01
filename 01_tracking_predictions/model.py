58849072f9a55e8e185c9f6be23db4b4031f9edc
Edward Kim <eddotman@gmail.com>
2022-04-01 15:54:39 -0400

    polish notebook 1

--- 01_tracking_predictions/model.py
+++ 01_tracking_predictions/model.py
@@ -4,5 +4,12 @@ class SynthesizabilityModel:
   def __init__(self):
     random.seed(123)
   
-  def predict_single(self, data_pt) -> float:
+  def predict_single(self, material:str) -> float:
+
+    if type(material) is not str:
+      raise TypeError("Materials must be string-based formulas!")
+    
+    # A "real" model would return a prediction from a trained model
+    # But this is a placeholder for a binary classifier, 
+    # because random.random will still return results on the interval [0,1]
     return random.random()
